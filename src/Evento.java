b205f735b8fccd9fbfe8f5aebbf09d56e9812d4f
André de Paula Terceiro <andreterceiro@yahoo.com.br>
2024-09-02 22:37:08 -0300

    Evento Java class

---
+++ src/Evento.java
@@ -0,0 +1,49 @@
+public class Evento {
+    /**
+     * Name of the event
+     */
+    private String nome;
+
+    /**
+     * Attraction of the event
+     */
+    private String atracao;
+
+    /**
+     * Constructor
+     * 
+     * @param nome    Name of the event
+     * @param atracao Attraction of the event
+     */
+    public Evento(String nome, String atracao) {
+        this.nome = nome;
+        this.atracao = atracao;
+    }
+
+    /**
+     * Getter of this.nome
+     * 
+     * @return this.nome
+     */
+    public String getNome() {
+        return this.nome;
+    }
+
+    /**
+     * Getter of this.atracao
+     *  
+     * @return this.atracao
+     */
+    public String getAtracao() {
+        return this.atracao;
+    }
+
+    /**
+     * Method that help in the task of transform an object of this class in a string
+     * 
+     * @return A string representation of an object of this class
+     */
+    public String toString() {
+        return "{Evento: nome='" + this.getNome() + "   , atracao='" + this.getAtracao() + "}";
+    }
+}
